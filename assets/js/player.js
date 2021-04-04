aec9eeeb46b0c84783763d13776a4421a2ee3d54
NedcloarBR <nedcloar1@hotmail.com>
2021-04-03 21:23:24 -0300

    Some Updates

--- assets/js/player.js
+++ assets/js/player.js
@@ -11,7 +11,7 @@ window.addEventListener("message", async e => {
 	console.log('[Crunchyroll Plus] Player encontrado!')
 
 	// Variáveis principais
-	const promises=[], request = [], s = [];
+	const promises=[], request = [];
 	const r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
 	for (let i in r) promises[i] = new Promise((resolve, reject) => request[i] = { resolve, reject });
 
@@ -25,9 +25,17 @@ window.addEventListener("message", async e => {
 	let video_stream_url = "";
 	let video_m3u8_array = [];
 	let video_mp4_array = [];
+	let final_translate = "";
 	let episode_title = "";
 	let rows_number = 0;
 	let sources = [];
+	
+	let dlSize = [];
+	let dlUrl = [];
+	for (let idx in r) {
+		dlSize[idx] = document.getElementById(r[idx] + "_down_size");
+		dlUrl[idx] = document.getElementById(r[idx] + "_down_url");
+	}
 
 	// Obter streams
 	for (let stream of video_config_media['streams']) {
@@ -42,7 +50,7 @@ window.addEventListener("message", async e => {
 					video_m3u8_array = video_mp4_array;
 					for (let i in r) {
 						const idx = i;
-						setTimeout(() => request[idx].resolve(), 200);
+						setTimeout(() => request[idx].resolve(), 400);
 					}
 					break;
 				}
@@ -78,7 +86,7 @@ window.addEventListener("message", async e => {
 		http.onreadystatechange = () => {
 			if (http.readyState == 4 && http.status == 200) {
 				fileSize = http.getResponseHeader('content-length');
-				if (!fileSize && !needs_proxy)
+				if (!fileSize)
 					return setTimeout(() => linkDownload(id), 5000);
 				else {
 					let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
@@ -86,15 +94,14 @@ window.addEventListener("message", async e => {
 					let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)));
 					if (i == 0) return console.log('addSource#i == 0');
 					let return_fileSize = (fileSize / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
-					s[id] = return_fileSize;
-					document.getElementById(r[id] + "_down_size").innerText = s[id];
+					dlSize[id].innerText = return_fileSize;
 					return console.log(`[Crunchyroll Plus] Source adicionado: ${r[id]} (${return_fileSize})`);
 				}
 			} else if (http.readyState == 4)
 				return setTimeout(() => linkDownload(id), 5000);
 		}
 		http.open("HEAD", video_mp4_url, true);
-		http.send();
+		http.send(null);
 	}
 
 	// Carregar player assim que encontrar as URLs dos m3u8.
@@ -145,16 +152,16 @@ window.addEventListener("message", async e => {
 			if (!didDownload) {
 				didDownload = true;
 				console.log('[Crunchyroll Plus] Baixando sources:')
-				for (id in r)
+				for (let id of [1,0,2,3,4])
 					linkDownload(id);
 			}
 		}
 		playerInstance.addButton(button_iconPath, button_tooltipText, download_ButtonClickAction, buttonId);
 
 		// Definir URL e Tamanho na lista de download
-		for (let id in r) {
-			document.getElementById(r[id] + "_down_url").href = video_mp4_array[id];
-			document.getElementById(r[id] + "_down_url").download = video_config_media['metadata']['title'].replaceAll(/[\\/<>\|?*:]+/g, '-').replaceAll('"', "'")
+		for (let id of [1,0,2,3,4]) {
+			dlUrl[id].href = video_mp4_array[id];
+			dlUrl[id].download = video_config_media['metadata']['title'];
 		}
 
 		// Funções para o player
@@ -170,7 +177,7 @@ window.addEventListener("message", async e => {
 			console.log(e)
 			if (e.code == 232011) {
 				jwplayer().load({
-					file: "https:// i.imgur.com/OufoM33.mp4"
+					file: "https://i.imgur.com/OufoM33.mp4"
 				});
 				jwplayer().setControls(false);
 				jwplayer().setConfig({
@@ -180,11 +187,11 @@ window.addEventListener("message", async e => {
 			}
 		});
 		
-		// Fica salvando o tempo do video a cada 5 segundos.
+		// Fica salvando o tempo do video a cada 7 segundos.
 		setInterval(() => {
 			if (jwplayer().getState() == "playing")
 				localStorage.setItem(video_id, jwplayer().getPosition());
-		}, 5000);
+		}, 7000);
 	}
 
 	/* ~~~~~~~~~~ FUNÇÕES ~~~~~~~~~~ */
@@ -221,7 +228,7 @@ window.addEventListener("message", async e => {
 	// ---- M3U8 ---- (assistir)
 	// Obtém o link direto pelo trailer
 	function getDirectStream(url, idx) {
-		setTimeout(() => request[idx].resolve(), 200);
+		setTimeout(() => request[idx].resolve(), 400);
 	}
 
 	// Obtém o link direto pelo padrão
@@ -235,7 +242,7 @@ window.addEventListener("message", async e => {
 		} else {
 			for (let i in r) {
 				const idx = i;
-				setTimeout(() => request[idx].reject('Manifest m3u8ListFromStream#master_m3u8.length === 0'), 200);
+				setTimeout(() => request[idx].reject('Manifest m3u8ListFromStream#master_m3u8.length === 0'), 400);
 			}
 			return [];
 		}
@@ -249,7 +256,7 @@ window.addEventListener("message", async e => {
 		res.push(buildM3u8(m3u8list));
 		for (let i in r) {
 			const idx = i;
-			setTimeout(() => request[idx].resolve(), 200);
+			setTimeout(() => request[idx].resolve(), 400);
 		}
 
 		return res;
